51b8aef39b01075dda26f51a36ceb0fa9d54920b
maksim_marynich <maksim_marynich@bitnet.by>
2019-10-30 11:09:49 +0300

    upd config

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
-  testRegex: '(/tests/.*|(\\.|/)(test|spec))\\.tsx?$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-};
